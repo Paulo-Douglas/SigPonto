16fdc9a559fb2ae06510927f0db997c5b1291186
Anderson Gabriel <andersong.pereiracruz@gmail.com>
2025-06-29 13:29:10 -0300

    feat: rota para tela de relatórios

--- src/frontend/lib/main.dart
+++ src/frontend/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:frontend/pages/pontos.dart';
 import './pages/login.dart';
 import './pages/home.dart';
 
@@ -20,6 +21,7 @@ class MyApp extends HookWidget {
       routes: {
         '/login': (context) => TelaLogin(height: height, width: width),
         '/home': (context) => TelaHome(),
+        '/pontos': (context) => TelaRelatorio(),
       },
     );
   }
