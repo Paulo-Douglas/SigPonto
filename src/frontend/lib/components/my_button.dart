835cbb7106365c799c6b59ad7617051d3089c5f2
Anderson Gabriel <andersong.pereiracruz@gmail.com>
2025-06-29 09:16:20 -0300

    feat: criando o component de botão

---
+++ src/frontend/lib/components/my_button.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:google_fonts/google_fonts.dart';
+import '../service/service.dart';
+
+class MyButton extends HookWidget {
+  final String actionButton;
+  final ValueNotifier<String> user;
+
+  const MyButton({super.key, required this.actionButton, required this.user});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () => Service(user: user.value),
+      style: ButtonStyle(
+        textStyle: WidgetStateProperty.resolveWith((states) {
+          return TextStyle(
+            fontFamily: GoogleFonts.alef().fontFamily,
+            color: Colors.red,
+            fontWeight: FontWeight.bold,
+          );
+        }),
+        foregroundColor: WidgetStateProperty.resolveWith((states) {
+          return Colors.red;
+        }),
+      ),
+      child: Text(actionButton, style: TextStyle(color: Colors.blueGrey)),
+    );
+  }
+}
