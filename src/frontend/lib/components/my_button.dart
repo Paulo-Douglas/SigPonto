cbefdc948bf12c94937afe5890cbe5a542e698a2
Anderson Gabriel <andersong.pereiracruz@gmail.com>
2025-06-29 20:40:51 -0300

    refactor: adaptando estilos do botão

--- src/frontend/lib/components/my_button.dart
+++ src/frontend/lib/components/my_button.dart
@@ -1,34 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:google_fonts/google_fonts.dart';
+import '../styles/button.dart';
 
 class MyButton extends HookWidget {
   final String actionButton;
   final VoidCallback onPressed;
+  final String styleButton;
 
   const MyButton({
     super.key,
     required this.actionButton,
     required this.onPressed,
+    this.styleButton = "",
   });
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
       onPressed: onPressed,
-      style: ButtonStyle(
-        textStyle: WidgetStateProperty.resolveWith((states) {
-          return TextStyle(
-            fontFamily: GoogleFonts.alef().fontFamily,
-            color: Colors.red,
-            fontWeight: FontWeight.bold,
-          );
-        }),
-        foregroundColor: WidgetStateProperty.resolveWith((states) {
-          return Colors.red;
-        }),
-      ),
-      child: Text(actionButton, style: TextStyle(color: Colors.blueGrey)),
+      style: styleButton.isNotEmpty
+          ? MyStyle.yellowButton
+          : MyStyle.greenButton,
+      child: Text(actionButton),
     );
   }
 }
