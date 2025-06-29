c869c5e2004b288cf6580f2bbbd4f2aa8f263f01
Anderson Gabriel <andersong.pereiracruz@gmail.com>
2025-06-29 13:46:50 -0300

    feat: base tela de relatórios

---
+++ src/frontend/lib/pages/pontos.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+class TelaRelatorio extends HookWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(title: Text("Relatórios")));
+  }
+}
